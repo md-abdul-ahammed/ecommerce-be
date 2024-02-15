56aff8351730484c1224d68ebde9cd0e51a64509
md-abdul-ahammed <mdabdulahammed1@gmail.com>
2024-02-15 19:23:25 +0600

    first commit

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,36 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-/* eslint-disable no-console */
 import { Server } from 'http'
 import app from './app'
 import config from './config'
-// import config from './config/index'
-
-let server: Server
+import { errorlogger, logger } from './shared/logger'
 
 async function bootstrap() {
-  try {
-    server = app.listen(config.port, () => {
-      console.log(`Application  listening on port ${config.port}`)
-    })
-  } catch (err) {
-    console.error('Failed to connect database', err)
+  const server: Server = app.listen(config.port, () => {
+    logger.info(`Server running on port ${config.port}`)
+  })
+
+  const exitHandler = () => {
+    if (server) {
+      server.close(() => {
+        logger.info('Server closed')
+      })
+    }
+    process.exit(1)
   }
+
+  const unexpectedErrorHandler = (error: unknown) => {
+    errorlogger.error(error)
+    exitHandler()
+  }
+
+  process.on('uncaughtException', unexpectedErrorHandler)
+  process.on('unhandledRejection', unexpectedErrorHandler)
+
+  process.on('SIGTERM', () => {
+    logger.info('SIGTERM received')
+    if (server) {
+      server.close()
+    }
+  })
 }
 
 bootstrap()
